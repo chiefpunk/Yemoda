f1a550a647801d2c4991804eb05d42eacc8287fd
risingstar <risingstar1002@mail.ru>
2020-04-28 01:51:33 +1000

    Finish submit action

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+/*global chrome*/
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 function App() {
+  const [domain, setDomain] = useState("");
+  const [success, setSuccess] = useState("");
+  const [fullUrl, setFullUrl] = useState("");
+  const now = new Date();
+  const utc_timestamp = Date.UTC(
+    now.getUTCFullYear(),
+    now.getUTCMonth(),
+    now.getUTCDate(),
+    now.getUTCHours(),
+    now.getUTCMinutes(),
+    now.getUTCSeconds(),
+    now.getUTCMilliseconds()
+  );
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const url = new URL(tabs[0].url);
+      setDomain(url.hostname);
+      setFullUrl(tabs[0].url);
+      // this.getHeadlines(domain);
+    });
+  }, []);
+  const onClick = () => {
+    axios
+      .post("http://youta-api.ngrok.io/starter-project", {
+        timestamp: utc_timestamp,
+        username: "user@user.com",
+        domain: domain,
+        url: fullUrl,
+      })
+      .then((response) => {
+        console.log(response);
+        //setData(response.data);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Container">
+        <h1>{domain}</h1>
+        <button onClick={() => onClick()} className="submit_button">
+          Submit
+        </button>
       </header>
     </div>
   );
